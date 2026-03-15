928377222a72c8e23ebaa73fdf95b56945cc08aa
LipatovCore <LipatovCore@yandex.ru>
2026-03-15 12:47:34 +0300

    feat(ui): адаптирован интерфейс под мобильные устройства (#14)

--- src/task/forms.py
+++ src/task/forms.py
@@ -1,14 +1,17 @@
 from django import forms
+
 from .models import Task
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title']
+        fields = ["title"]
         widgets = {
-            'title': forms.TextInput(attrs={
-                'placeholder': 'Введите задачу...',
-                'size': '60',
-            })
+            "title": forms.TextInput(
+                attrs={
+                    "class": "task-input",
+                    "placeholder": "Введите задачу...",
+                }
+            )
         }
